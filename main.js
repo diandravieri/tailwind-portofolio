f0ed29fe92154acf7de2d60e4efd9f185cf500db
ww-ahyuda <wahyuda542@smk.belajar.id>
2024-09-05 10:38:50 +0700

    CHANGE_THEME

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 import "./style.css";
+const ELE = document.documentElement;
+
+
+// Funcition untuk ganti tema
+document.querySelectorAll(".theme-switch").forEach((item) =>
+    item.addEventListener("click", () => {
+      ELE.classList.toggle("dark");
+      ELE.classList.contains("dark")
+        ? localStorage.setItem("theme", "dark")
+        : localStorage.setItem("theme", "light");
+    })
+  );
+
+
+  ELE.classList.add(localStorage.getItem("theme"));
