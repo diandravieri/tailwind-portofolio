5d28a644a6692aed4a794e76080a0bb077d81cbe
ww-ahyuda <wahyuda542@smk.belajar.id>
2024-09-05 10:59:02 +0700

    RESPONSIVE_MOBILE-NAV-2

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 import "./style.css";
+
 const ELE = document.documentElement;
+const mobileNav = document.getElementById("mobile-nav");
+const menuBar = document.querySelector("#menubar");
+const menuBarIcon = document.querySelector("#menubar i");
 
-
-// Funcition untuk ganti tema
+// funsi untuk mengganti tema
 document.querySelectorAll(".theme-switch").forEach((item) =>
-    item.addEventListener("click", () => {
-      ELE.classList.toggle("dark");
-      ELE.classList.contains("dark")
-        ? localStorage.setItem("theme", "dark")
-        : localStorage.setItem("theme", "light");
-    })
-  );
+  item.addEventListener("click", () => {
+    ELE.classList.toggle("dark");
+    ELE.classList.contains("dark")
+      ? localStorage.setItem("theme", "dark")
+      : localStorage.setItem("theme", "light");
+  })
+);
 
 
-  ELE.classList.add(localStorage.getItem("theme"));
+//responsive NAVBAR
+const options = [menuBar];
+
+options.forEach((item) =>
+  item.addEventListener("click", () => {
+    mobileNav.classList.toggle("h-0");
+    mobileNav.classList.toggle("h-96");
+    menuBarIcon.classList.toggle("fa-xmark");
+    menuBarIcon.classList.toggle("fa-bars");    
+  })
+);
+
+ELE.classList.add(localStorage.getItem("theme"));
